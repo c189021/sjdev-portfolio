d7278e954009a3b9191d7e61436736802bcee69d
SeoungJin Park <sjjw1111@naver.com>
2026-01-19 14:10:39 +0900

    fix: update base path in vite.config.ts for correct deployment

--- sjdev-portfolio/vite.config.ts
+++ sjdev-portfolio/vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/sjdev-portfolio/", // 👈 이 줄이 반드시 있어야 합니다!
+  base: "/", // 👈 이 줄이 반드시 있어야 합니다!
 });
